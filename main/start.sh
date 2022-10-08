389457eac7db6ad3a72d3ecea90449fbaad3bad8
airtcy <103116133+airtcy@users.noreply.github.com>
2022-10-08 02:47:32 +0000

    Update start.sh

--- main/start.sh
+++ main/start.sh
@@ -1,5 +1,5 @@
 export PORT=${PORT-8080}
-export UUID=${UUID-1eb6e917774b4a84aff6b058577c60a5}
+export UUID=${UUID-41d4cec7-a7cf-4d38-aaf4-bf896eec3aaa}
 export PATH_vless=${PATH_vless-/vless/$UUID}
 export PATH_trojan=${PATH_trojan-/trojan/$UUID}
 export PATH_vmess=${PATH_vmess-/vmess/$UUID}
